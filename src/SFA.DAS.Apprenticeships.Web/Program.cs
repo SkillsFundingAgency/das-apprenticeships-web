7d21a657295b123c31d37644fd78d2ef25cb4282
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-11-03 13:07:28 +0000

    Change of price UI

--- src/SFA.DAS.Apprenticeships.Web/Program.cs
+++ src/SFA.DAS.Apprenticeships.Web/Program.cs
@@ -1,9 +1,10 @@
-using System.Diagnostics.CodeAnalysis;
-using SFA.DAS.Provider.Shared.UI.Startup;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Apprenticeships.Web.AppStart;
+using SFA.DAS.Apprenticeships.Web.Validators;
 using SFA.DAS.Provider.Shared.UI.Models;
-using SFA.DAS.Provider.Shared.UI;
+using SFA.DAS.Provider.Shared.UI.Startup;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SFA.DAS.Apprenticeships.Web
 {
@@ -51,6 +52,7 @@ namespace SFA.DAS.Apprenticeships.Web
                         options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
                     }
                 })
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateChangeOfPriceModelValidator>())
                 //TODO: Figure out what NavigationSection is and whether we need it
                 //.SetDefaultNavigationSection(NavigationSection.Home)
                 .EnableGoogleAnalytics()
