f487ef6796f6be98239c95d1367df2581f34e741
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-30 09:06:39 +0000

    CreateApprenticeshipPriceHistoryRequest

---
+++ src/SFA.DAS.Apprenticeships.Domain/Apprenticeships/Api/Requests/CreateApprenticeshipPriceHistory.cs
@@ -0,0 +1,28 @@
+﻿using SFA.DAS.Apprenticeships.Domain.Interfaces;
+
+namespace SFA.DAS.Apprenticeships.Domain.Apprenticeships.Api.Requests;
+
+public class CreateApprenticeshipPriceHistoryRequest : IPostApiRequest
+{
+    private readonly Guid _apprenticeshipKey;
+
+    public CreateApprenticeshipPriceHistoryRequest(Guid apprenticeshipKey, CreateApprenticeshipPriceHistoryData data)
+    {
+        _apprenticeshipKey = apprenticeshipKey;
+        Data = data;
+    }
+
+    public string PostUrl => $"{_apprenticeshipKey}/priceHistory";
+    public object Data { get; set; }
+}
+
+public class CreateApprenticeshipPriceHistoryData
+{
+    public long? ProviderId { get; set; }
+    public long? EmployerId { get; set; }
+    public string UserId { get; set; }
+    public decimal? TrainingPrice { get; set; }
+    public decimal? AssessmentPrice { get; set; }
+    public decimal? TotalPrice { get; set; }
+    public string Reason { get; set; }
+}
