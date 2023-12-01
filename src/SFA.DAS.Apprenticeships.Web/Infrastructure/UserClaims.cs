b1f02370181cc0ca47fe3971085b352500d458a1
dinathain <55487881+dinathain@users.noreply.github.com>
2023-12-01 15:06:55 +0000

    Change authentication type claim name

--- src/SFA.DAS.Apprenticeships.Web/Infrastructure/UserClaims.cs
+++ src/SFA.DAS.Apprenticeships.Web/Infrastructure/UserClaims.cs
@@ -5,5 +5,5 @@ namespace SFA.DAS.Apprenticeships.Web.Infrastructure;
 [ExcludeFromCodeCoverage]
 public static class UserClaims
 {
-    public static string AuthenticationType => "http://schemas.portal.com/authtype";
+    public static string AuthenticationType => "AuthenticationType";
 }
