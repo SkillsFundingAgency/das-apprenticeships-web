6e4264ec1e0b5da605858d30c1563e42a0ecd466
Michael Young <mick.young1981@googlemail.com>
2024-01-11 09:54:02 +0000

    Remove local.settings.json

--- src/SFA.DAS.Apprenticeships.Web/Program.cs
+++ src/SFA.DAS.Apprenticeships.Web/Program.cs
@@ -55,7 +55,6 @@ namespace SFA.DAS.Apprenticeships.Web
 			builder.Services.AddApplicationInsightsTelemetry();
 
 			// Config
-			config.AddJsonFile("local.settings.json", optional: true);
 			builder.ConfigureAzureTableStorage(config);
 			config.ValidateConfiguration();
 			builder.AddDistributedCache(config);
