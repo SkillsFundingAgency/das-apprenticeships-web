669ed543d8b82cc4e73f90ec1f14719c4b95cb26
Michael Young <mick.young1981@googlemail.com>
2024-01-18 10:56:52 +0000

    Remove cdn from app settings

--- src/SFA.DAS.Apprenticeships.Web/Extensions/ClaimsPrincipalExtensions.cs
+++ src/SFA.DAS.Apprenticeships.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,8 +1,10 @@
-﻿using System.Security.Claims;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
 using SFA.DAS.Apprenticeships.Web.Infrastructure;
 
 namespace SFA.DAS.Apprenticeships.Web.Extensions;
 
+[ExcludeFromCodeCoverage]
 public static class ClaimsPrincipalExtensions
 {
     public static string GetUserId(this ClaimsPrincipal user)
