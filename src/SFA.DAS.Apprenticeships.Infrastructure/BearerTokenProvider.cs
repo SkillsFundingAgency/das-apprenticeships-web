ba7d9552791049610b38768300b0ff23efcde302
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2024-01-22 11:40:01 +0000

    Code review fixes

--- src/SFA.DAS.Apprenticeships.Infrastructure/BearerTokenProvider.cs
+++ src/SFA.DAS.Apprenticeships.Infrastructure/BearerTokenProvider.cs
@@ -5,11 +5,10 @@ using System.Security.Claims;
 
 namespace SFA.DAS.Apprenticeships.Infrastructure
 {
-	//  Its not intended for this class to remain in this project, it will be moved to a shared project
 	public static class BearerTokenProvider
 	{
 		private static string _signingKey = string.Empty;
-		private static int _expiryTime = 5;
+		private const int _expiryTimeMinutes = 5;
 
 		/// <summary>
 		/// Set the bearer token signing key, must exceed 128bits in length
@@ -18,37 +17,29 @@ namespace SFA.DAS.Apprenticeships.Infrastructure
 		{
 			if (string.IsNullOrEmpty(signingKey))
 			{
-				throw new BearTokenException("Signing key cannot be null or empty");
+				throw new BearerTokenException("Signing key cannot be null or empty");
 			}
 
 			const int minimumKeySize = 128;
 			if (signingKey.Length * 8 < minimumKeySize)
 			{
 				// This checks the key is at least 128 bits long, otherwise the token will fail to be generated
-				throw new BearTokenException("Signing key must exceed 128bits in length");
+				throw new BearerTokenException("Signing key must exceed 128bits in length");
 			}
 
 			_signingKey = signingKey;
-		}
-
-		/// <summary>
-		/// (Optional) Set bearer token expiry time, default is 5 minutes 
-		/// </summary>
-		public static void SetExpiryTime(int expiryTime)
-		{
-			_expiryTime = expiryTime;
 		}
 
 		public static string GetBearerToken(this HttpContext httpContext)
 		{
 			if (string.IsNullOrEmpty(_signingKey))
 			{
-				throw new BearTokenException("Signing key must be set before a token can be retrieved. This should ideally be done in startup");
+				throw new BearerTokenException("Signing key must be set before a token can be retrieved. This should ideally be done in startup");
 			}
 
 			var user = httpContext.User;
 			if (!IsUserAuthenticated(user))
-				throw new BearTokenException("User is not authenticated, cannot create token");
+				throw new BearerTokenException("User is not authenticated, cannot create token");
 
 			var key = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(_signingKey));
 			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
@@ -56,7 +47,7 @@ namespace SFA.DAS.Apprenticeships.Infrastructure
 			var token = new JwtSecurityToken(
 				claims: user.Claims,
 				signingCredentials: creds,
-				expires: DateTime.UtcNow.AddMinutes(_expiryTime)
+				expires: DateTime.UtcNow.AddMinutes(_expiryTimeMinutes)
 			);
 
 			return new JwtSecurityTokenHandler().WriteToken(token);
@@ -73,10 +64,9 @@ namespace SFA.DAS.Apprenticeships.Infrastructure
 		}
 	}
 
-	// Although this could be in a seperate file, all the code used here will get moved to a seperate shared project
-	public class BearTokenException : Exception
+	public class BearerTokenException : Exception
 	{
-		public BearTokenException(string message) : base(message)
+		public BearerTokenException(string message) : base(message)
 		{
 		}
 	}
