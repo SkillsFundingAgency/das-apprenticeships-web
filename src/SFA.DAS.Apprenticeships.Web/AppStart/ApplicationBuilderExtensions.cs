21dbaf14ef08ce170ba2213e8a152dae2f9bf527
Michael Young <mick.young1981@googlemail.com>
2024-01-22 13:17:37 +0000

    Allow co browsing script

--- src/SFA.DAS.Apprenticeships.Web/AppStart/ApplicationBuilderExtensions.cs
+++ src/SFA.DAS.Apprenticeships.Web/AppStart/ApplicationBuilderExtensions.cs
@@ -11,11 +11,11 @@ public static class ApplicationBuilderExtensions
 		app.Use(async (context, next) =>
 		{
 			context.Response.Headers["Content-Security-Policy"] =
-				"default-src 'self' 'unsafe-inline' https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com *.google-analytics.com; " +
+                "default-src 'self' 'unsafe-inline' https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com *.google-analytics.com https://*.rcrsv.io; " +
 				"style-src 'self' 'unsafe-inline' https://*.azureedge.net; " +
-				"img-src 'self' https://*.azureedge.net *.google-analytics.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
+                "img-src 'self' https://*.azureedge.net *.google-analytics.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io; " +
 				$"script-src 'self' 'unsafe-inline' {cdn} " +
-				"*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
+                "*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io; " +
 				$"font-src 'self' {cdn} data:;";
 			await next();
 
