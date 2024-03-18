ebd5ce0a239988cd6eb5cdf2df42a35daf9f909a
tom-gough <tom.gough.tgsd@outlook.com>
2024-03-18 14:47:24 +0000

    FLP-399 validation

---
+++ src/SFA.DAS.Apprenticeships.Web/Validators/ProviderConfirmPriceBreakdownPriceChangeModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using SFA.DAS.Apprenticeships.Web.Models.ChangeOfPrice;
+
+namespace SFA.DAS.Apprenticeships.Web.Validators;
+
+public class ProviderConfirmPriceBreakdownPriceChangeModelValidator : AbstractValidator<ProviderConfirmPriceBreakdownPriceChangeModel>
+{
+    public ProviderConfirmPriceBreakdownPriceChangeModelValidator()
+    {
+        RuleFor(x => x)
+            .Must(BreakdownMatchRequestedTotalPrice)
+            .WithName(nameof(ProviderConfirmPriceBreakdownPriceChangeModel.ApprenticeshipTrainingPrice))
+            .WithMessage("The new training price and end-point assessment must match the total price requested by the employer.");
+    }
+
+    private bool BreakdownMatchRequestedTotalPrice(ProviderConfirmPriceBreakdownPriceChangeModel model)
+    {
+        if (model.ApprenticeshipTrainingPrice + model.ApprenticeshipEndPointAssessmentPrice != model.ApprenticeshipTotalPrice)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
