f9ee8e3b2964732ae57c57838ca99823776d6818
tom-gough <tom.gough.tgsd@outlook.com>
2024-04-22 11:24:28 +0100

    FLP-606 unit test simplification

--- src/SFA.DAS.Apprenticeships.Web.UnitTests/Validators/ChangeOfStartDate/ProviderChangeOfStartDateModelValidatorTests.cs
+++ src/SFA.DAS.Apprenticeships.Web.UnitTests/Validators/ChangeOfStartDate/ProviderChangeOfStartDateModelValidatorTests.cs
@@ -2,25 +2,39 @@
 using SFA.DAS.Apprenticeships.Web.Models;
 using SFA.DAS.Apprenticeships.Web.Models.ChangeOfStartDate;
 using SFA.DAS.Apprenticeships.Web.Validators.ChangeOfStartDate;
-using System;
 
 namespace SFA.DAS.Apprenticeships.Web.UnitTests.Validators.ChangeOfStartDate;
 
 [TestFixture]
 public class ProviderChangeOfStartDateModelValidatorTests
 {
-    private const string _noChangeMessage = "You must change the actual training start date";
-    private const string _earliestStartDateMessage = "The new start date must be no earlier than ";
-    private const string _latestStartDateMessage = "The new start date must be no later than ";
-    private const string _lastFridayOfSchoolMessage = "The start date must be after {0} when the learner has reached school leaving age.";
-    private const string _standardEarliestDateMessage = "This training course is only available to apprentices with a start date after ";
-    private const string _standardLatestDateMessage = "This training course is only available to apprentices with a start date before ";
-    private const string _standardVersionEarliestDateMessage = "This version of the training course is only available to apprentices with a start date after ";
-    private const string _standardVersionLatestDateMessage = "This version of the training course is only available to apprentices with a start date before ";
+    private const string NoChangeMessage = "You must change the actual training start date";
+    private const string EarliestStartDateMessage = "The new start date must be no earlier than ";
+    private const string LatestStartDateMessage = "The new start date must be no later than ";
+    private const string LastFridayOfSchoolMessage = "The start date must be after {0} when the learner has reached school leaving age.";
+    private const string StandardEarliestDateMessage = "This training course is only available to apprentices with a start date after ";
+    private const string StandardLatestDateMessage = "This training course is only available to apprentices with a start date before ";
+    private const string StandardVersionEarliestDateMessage = "This version of the training course is only available to apprentices with a start date after ";
+    private const string StandardVersionLatestDateMessage = "This version of the training course is only available to apprentices with a start date before ";
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_StartDate_ReturnsExpectedErrorMessage(bool startDateChanged)
+    [Test]
+    public void Validate_Valid_DoesNotReturnMessage()
+    {
+        // Arrange
+        var originalStartDate = new DateTime(2023, 6, 1);
+        var model = BuildValidTestModel(originalStartDate);
+
+        var validator = new ProviderChangeOfStartDateModelValidator();
+
+        // Act
+        var result = validator.Validate(model);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Test]
+    public void Validate_StartDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
@@ -29,7 +43,7 @@ public class ProviderChangeOfStartDateModelValidatorTests
         {
             Year = originalStartDate.Year,
             Month = originalStartDate.Month,
-            Day = startDateChanged ? originalStartDate.Day + 1 : originalStartDate.Day
+            Day = originalStartDate.Day
         };
 
         var validator = new ProviderChangeOfStartDateModelValidator();
@@ -38,25 +52,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (startDateChanged)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == _noChangeMessage);
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == NoChangeMessage);
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_EarliestStartDate_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_EarliestStartDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.EarliestStartDate = valid ? originalStartDate : originalStartDate.AddDays(2);
+        model.EarliestStartDate = originalStartDate.AddDays(2);
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -64,25 +70,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == $"{_earliestStartDateMessage}{model.EarliestStartDate.Value:dd MM yyyy}.");
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == $"{EarliestStartDateMessage}{model.EarliestStartDate.Value:dd MM yyyy}.");
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_LatestStartDate_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_LatestStartDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.LatestStartDate = valid ? originalStartDate.AddDays(1) : originalStartDate;
+        model.LatestStartDate = originalStartDate;
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -90,25 +88,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == $"{_latestStartDateMessage}{model.LatestStartDate.Value:dd MM yyyy}.");
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == $"{LatestStartDateMessage}{model.LatestStartDate.Value:dd MM yyyy}.");
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_LastFridayOfSchool_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_LastFridayOfSchool_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.LastFridayOfSchool = valid ? originalStartDate : originalStartDate.AddDays(2);
+        model.LastFridayOfSchool = originalStartDate.AddDays(2);
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -116,25 +106,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == String.Format(_lastFridayOfSchoolMessage, model.LastFridayOfSchool.ToString("dd MM yyyy")));
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == String.Format(LastFridayOfSchoolMessage, model.LastFridayOfSchool.ToString("dd MM yyyy")));
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_StandardEarliestDate_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_StandardEarliestDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.StandardEarliestDate = valid ? originalStartDate : originalStartDate.AddDays(2);
+        model.StandardEarliestDate = originalStartDate.AddDays(2);
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -142,25 +124,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == $"{_standardEarliestDateMessage}{model.StandardEarliestDate.Value:dd MM yyyy}.");
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == $"{StandardEarliestDateMessage}{model.StandardEarliestDate.Value:dd MM yyyy}.");
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_StandardLatestDate_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_StandardLatestDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.StandardLatestDate = valid ? originalStartDate.AddDays(1) : originalStartDate;
+        model.StandardLatestDate = originalStartDate;
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -168,25 +142,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == $"{_standardLatestDateMessage}{model.StandardLatestDate.Value:dd MM yyyy}.");
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == $"{StandardLatestDateMessage}{model.StandardLatestDate.Value:dd MM yyyy}.");
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_StandardVersionEarliestDate_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_StandardVersionEarliestDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.StandardVersionEarliestDate = valid ? originalStartDate : originalStartDate.AddDays(2);
+        model.StandardVersionEarliestDate = originalStartDate.AddDays(2);
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -194,25 +160,17 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == $"{_standardVersionEarliestDateMessage}{model.StandardVersionEarliestDate.Value:dd MM yyyy}.");
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == $"{StandardVersionEarliestDateMessage}{model.StandardVersionEarliestDate.Value:dd MM yyyy}.");
     }
 
-    [TestCase(true)]
-    [TestCase(false)]
-    public void Validate_StandardVersionLatestDate_ReturnsExpectedErrorMessage(bool valid)
+    [Test]
+    public void Validate_StandardVersionLatestDate_ReturnsExpectedErrorMessage()
     {
         // Arrange
         var originalStartDate = new DateTime(2023, 6, 1);
         var model = BuildValidTestModel(originalStartDate);
-        model.StandardVersionLatestDate = valid ? originalStartDate.AddDays(1) : originalStartDate;
+        model.StandardVersionLatestDate = originalStartDate;
 
         var validator = new ProviderChangeOfStartDateModelValidator();
 
@@ -220,15 +178,8 @@ public class ProviderChangeOfStartDateModelValidatorTests
         var result = validator.Validate(model);
 
         // Assert
-        if (valid)
-        {
-            result.IsValid.Should().BeTrue();
-        }
-        else
-        {
-            result.IsValid.Should().BeFalse();
-            result.Errors.Should().Contain(x => x.ErrorMessage == $"{_standardVersionLatestDateMessage}{model.StandardVersionLatestDate.Value:dd MM yyyy}.");
-        }
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(x => x.ErrorMessage == $"{StandardVersionLatestDateMessage}{model.StandardVersionLatestDate.Value:dd MM yyyy}.");
     }
 
     [Test]
@@ -247,7 +198,7 @@ public class ProviderChangeOfStartDateModelValidatorTests
 
         // Assert
         result.IsValid.Should().BeFalse();
-        result.Errors.Should().NotContain(x => x.ErrorMessage == $"{_standardVersionEarliestDateMessage}{model.StandardVersionEarliestDate.Value:dd MM yyyy}.");
+        result.Errors.Should().NotContain(x => x.ErrorMessage == $"{StandardVersionEarliestDateMessage}{model.StandardVersionEarliestDate.Value:dd MM yyyy}.");
     }
 
     [Test]
@@ -266,7 +217,7 @@ public class ProviderChangeOfStartDateModelValidatorTests
 
         // Assert
         result.IsValid.Should().BeFalse();
-        result.Errors.Should().NotContain(x => x.ErrorMessage == $"{_standardVersionLatestDateMessage}{model.StandardVersionLatestDate.Value:dd MM yyyy}.");
+        result.Errors.Should().NotContain(x => x.ErrorMessage == $"{StandardVersionLatestDateMessage}{model.StandardVersionLatestDate.Value:dd MM yyyy}.");
     }
 
     private ProviderChangeOfStartDateModel BuildValidTestModel(DateTime originalStartDate)
