8f1490380a3a04f6020d154cff7c630b317ee7dc
tom-gough <tom.gough.tgsd@outlook.com>
2024-05-09 11:11:45 +0100

    fix request location

--- src/SFA.DAS.Apprenticeships.Domain/Apprenticeships/Api/Requests/ApprovePendingStartDateChangeRequest.cs
+++ src/SFA.DAS.Apprenticeships.Domain/Apprenticeships/Api/Requests/ApprovePendingStartDateChangeRequest.cs
@@ -2,26 +2,24 @@
 
 namespace SFA.DAS.Apprenticeships.Domain.Apprenticeships.Api.Requests;
 
-public class ApprovePendingPriceChangeRequest : IPatchApiRequest
+public class ApprovePendingStartDateChangeRequest : IPatchApiRequest
 {
 	private readonly Guid _apprenticeshipKey;
 
-	public ApprovePendingPriceChangeRequest(Guid apprenticeshipKey, ApprovePendingPriceChangeData data)
+	public ApprovePendingStartDateChangeRequest(Guid apprenticeshipKey, ApprovePendingPriceChangeData data)
 	{
 		_apprenticeshipKey = apprenticeshipKey;
 		Data = data;
 	}
 
-	public string PatchUrl => $"Apprenticeship/{_apprenticeshipKey}/priceHistory/pending/approve";
+	public string PatchUrl => $"Apprenticeship/{_apprenticeshipKey}/startDateChange/pending/approve";
 	public object Data { get; set; }
-    public bool SendBearerToken => true;
+	public bool SendBearerToken => true;
 }
 
-public class ApprovePendingPriceChangeData
+public class ApprovePendingStartDateChangeData
 {
 #pragma warning disable CS8618 // Non-nullable field is uninitialized. This will be set when constructed
 	public string UserId { get; set; }
-    public decimal? TrainingPrice { get; set; }
-    public decimal? AssessmentPrice { get; set; }
 #pragma warning restore CS8618
 }
