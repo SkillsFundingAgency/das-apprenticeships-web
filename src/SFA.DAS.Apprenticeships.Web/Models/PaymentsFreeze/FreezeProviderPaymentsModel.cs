bf6652d255e45ec69d656792ac722b5eb126c734
tom-gough <tom.gough.tgsd@outlook.com>
2024-06-05 08:25:20 +0100

    FLP-699 file scoped namespaces

--- src/SFA.DAS.Apprenticeships.Web/Models/PaymentsFreeze/FreezeProviderPaymentsModel.cs
+++ src/SFA.DAS.Apprenticeships.Web/Models/PaymentsFreeze/FreezeProviderPaymentsModel.cs
@@ -1,12 +1,11 @@
-﻿namespace SFA.DAS.Apprenticeships.Web.Models.PaymentsFreeze
+﻿namespace SFA.DAS.Apprenticeships.Web.Models.PaymentsFreeze;
+
+public class FreezeProviderPaymentsModel
 {
-    public class FreezeProviderPaymentsModel
-    {
-        public string EmployerAccountId { get; set; }
-        public string? ApprenticeshipHashedId { get; set; }
-        public string BackLinkUrl { get; set; }
-        public Guid ApprenticeshipKey { get; set; }
-        public string? ReasonForFreeze { get; set; }
-        public bool? FreezePayments { get; set; }
-    }
+    public string EmployerAccountId { get; set; }
+    public string? ApprenticeshipHashedId { get; set; }
+    public string BackLinkUrl { get; set; }
+    public Guid ApprenticeshipKey { get; set; }
+    public string? ReasonForFreeze { get; set; }
+    public bool? FreezePayments { get; set; }
 }
