bc1db8a77a6a07afa773b39f5594c6d1e4fa2a04
tom-gough <tom.gough.tgsd@outlook.com>
2024-06-05 08:17:21 +0100

    FLP-699 Indentation & file scoped namespace fix

--- src/SFA.DAS.Apprenticeships.Web/Controllers/PaymentsFreeze/EmployerPaymentsFreezeController.cs
+++ src/SFA.DAS.Apprenticeships.Web/Controllers/PaymentsFreeze/EmployerPaymentsFreezeController.cs
@@ -5,51 +5,50 @@ using SFA.DAS.Apprenticeships.Web.Infrastructure;
 using SFA.DAS.Employer.Shared.UI;
 using SFA.DAS.Apprenticeships.Web.Models.PaymentsFreeze;
 
-namespace SFA.DAS.Apprenticeships.Web.Controllers.PaymentsFreeze
+namespace SFA.DAS.Apprenticeships.Web.Controllers.PaymentsFreeze;
+
+[Authorize]
+[Route("employer/{employerAccountId}/PaymentsFreeze/{apprenticeshipHashedId}")]
+public class EmployerPaymentsFreezeController : Controller
 {
-	[Authorize]
-	[Route("employer/{employerAccountId}/PaymentsFreeze/{apprenticeshipHashedId}")]
-	public class EmployerPaymentsFreezeController : Controller
-	{
-		private readonly ILogger<EmployerPaymentsFreezeController> _logger;
-		private readonly IApprenticeshipService _apprenticeshipService;
-		private readonly UrlBuilder _externalEmployerUrlHelper;
+    private readonly ILogger<EmployerPaymentsFreezeController> _logger;
+    private readonly IApprenticeshipService _apprenticeshipService;
+    private readonly UrlBuilder _externalEmployerUrlHelper;
 
-        public const string FreezeProviderPaymentsViewName = "~/Views/PaymentsFreeze/FreezeProviderPayments.cshtml";
+    public const string FreezeProviderPaymentsViewName = "~/Views/PaymentsFreeze/FreezeProviderPayments.cshtml";
 
-        public EmployerPaymentsFreezeController(ILogger<EmployerPaymentsFreezeController> logger, IApprenticeshipService apprenticeshipService, UrlBuilder externalEmployerUrlHelper)
-		{
-			_logger = logger;
-			_apprenticeshipService = apprenticeshipService;
-			_externalEmployerUrlHelper = externalEmployerUrlHelper;
-		}
+    public EmployerPaymentsFreezeController(ILogger<EmployerPaymentsFreezeController> logger, IApprenticeshipService apprenticeshipService, UrlBuilder externalEmployerUrlHelper)
+    {
+        _logger = logger;
+        _apprenticeshipService = apprenticeshipService;
+        _externalEmployerUrlHelper = externalEmployerUrlHelper;
+    }
 
-		[HttpGet]
-		[Authorize(Policy = nameof(PolicyNames.HasEmployerAccount))]
-		[Route("")]
-		public async Task<IActionResult> FreezeProviderPaymentsPage(string employerAccountId, string apprenticeshipHashedId)
-		{
-			var response = await _apprenticeshipService.GetApprenticeshipKey(apprenticeshipHashedId);
-			if (response == Guid.Empty)
-			{
-				return NotFound();
-			}
-
-			var backLink = _externalEmployerUrlHelper.CommitmentsV2Link("ApprenticeDetails", employerAccountId, apprenticeshipHashedId.ToUpper());
-
-            var model = new FreezeProviderPaymentsModel{ ApprenticeshipKey = response, ApprenticeshipHashedId = apprenticeshipHashedId, BackLinkUrl = backLink, EmployerAccountId = employerAccountId };
-
-            return View(FreezeProviderPaymentsViewName, model);
-        }
-
-        [HttpPost]
-        [Authorize(Policy = nameof(PolicyNames.HasEmployerAccount))]
-        [Route("")]
-        public async Task<IActionResult> FreezeProviderPaymentsPage(FreezeProviderPaymentsModel model)
+    [HttpGet]
+    [Authorize(Policy = nameof(PolicyNames.HasEmployerAccount))]
+    [Route("")]
+    public async Task<IActionResult> FreezeProviderPaymentsPage(string employerAccountId, string apprenticeshipHashedId)
+    {
+        var response = await _apprenticeshipService.GetApprenticeshipKey(apprenticeshipHashedId);
+        if (response == Guid.Empty)
         {
-            await _apprenticeshipService.FreezePayments(model.ApprenticeshipKey, model.ReasonForFreeze);
-
-            return Redirect(_externalEmployerUrlHelper.CommitmentsV2Link("ApprenticeDetails", model.EmployerAccountId, model.ApprenticeshipHashedId?.ToUpper()) + "?showProviderPaymentsInactive=true");
+            return NotFound();
         }
+
+        var backLink = _externalEmployerUrlHelper.CommitmentsV2Link("ApprenticeDetails", employerAccountId, apprenticeshipHashedId.ToUpper());
+
+        var model = new FreezeProviderPaymentsModel{ ApprenticeshipKey = response, ApprenticeshipHashedId = apprenticeshipHashedId, BackLinkUrl = backLink, EmployerAccountId = employerAccountId };
+
+        return View(FreezeProviderPaymentsViewName, model);
+    }
+
+    [HttpPost]
+    [Authorize(Policy = nameof(PolicyNames.HasEmployerAccount))]
+    [Route("")]
+    public async Task<IActionResult> FreezeProviderPaymentsPage(FreezeProviderPaymentsModel model)
+    {
+        await _apprenticeshipService.FreezePayments(model.ApprenticeshipKey, model.ReasonForFreeze);
+
+        return Redirect(_externalEmployerUrlHelper.CommitmentsV2Link("ApprenticeDetails", model.EmployerAccountId, model.ApprenticeshipHashedId?.ToUpper()) + "?showProviderPaymentsInactive=true");
     }
 }
